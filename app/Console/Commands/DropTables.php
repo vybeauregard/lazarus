dc1820e005841323809082a4f6efa2424857ea52
Trey Beauregard <vybeauregard@alumni.ou.edu>
2017-12-23 10:48:53 -0500

    Add drop table command to init script

---
+++ app/Console/Commands/DropTables.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Console\Commands;
+
+use DB;
+use Schema;
+use Illuminate\Console\Command;
+
+class DropTables extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:drop-tables';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Drop all tables from the database. FOR DEPLOYMENT ONLY';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        foreach(DB::select('SHOW TABLES') as $table) {
+            $table_array = get_object_vars($table);
+            Schema::drop($table_array[key($table_array)]);
+        }
+    }
+}
