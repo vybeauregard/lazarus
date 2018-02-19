4c29cce6a67945ea82de957ff214cb298bdb2ff1
Trey Beauregard <vybeauregard@alumni.ou.edu>
2018-02-19 15:24:30 -0500

    Nav is bootstrap 4-friendly

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="{{ app()->getLocale() }}">
-<head>
+  <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,67 +15,48 @@
     <link href="{{ mix('/css/vendor.css') }}" rel="stylesheet">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
     @yield('custom-css')
-</head>
-<body>
+
+  </head>
+  <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+          <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              @guest
+              <li class="nav-item {{ URL::current() == route('login') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('login') }}">Login <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item {{ URL::current() == route('register') ? 'active' : '' }}">
+                <a class="nav-link" href="{{ route('register') }}">Register</a>
+              </li>
+              @else
+                @include('layouts.nav')
+              <li class="nav-item dropdown {{ URL::current() == route('users.index') ? 'active' : '' }}">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  {{ Auth::user()->name }}
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a class="dropdown-item" href="{{ route('users.index') }}">Admin</a>
+                  <a href="{{ route('logout') }}"
+                     class="dropdown-item"
+                      onclick="event.preventDefault();
+                               document.getElementById('logout-form').submit();">
+                      Logout
+                  </a>
 
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+                  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                      {{ csrf_field() }}
+                  </form>
                 </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            @include('layouts.nav')
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    @if(Auth::user()->isAdmin())
-                                        <li><a href="{{ route('users.index') }}">Admin</a></li>
-                                    @endif
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
+              </li>
+              @endguest
+            </ul>
+          </div>
         </nav>
 
         <div class="body">
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,5 +1,10 @@
 @foreach(config('menus') as $menu => $visible)
     @if($visible)
-    <li><a href="{{ route($menu . '.index') }}">{{ ucwords($menu) }}</a></li>
+    <li class="nav-item {{ Request::segments()[0] == $menu ? 'active' : '' }}">
+        <a href="{{ route($menu . '.index') }}" class="nav-link">{{ ucwords($menu) }}
+            @if(Request::segments()[0] == $menu)
+            <span class="sr-only">(current)</span>
+            @endif
+        </a></li>
     @endif
 @endforeach
