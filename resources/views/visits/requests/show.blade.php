b28323085eb4c9123b359ca6e3bf1090b21c9114
Trey Beauregard <vybeauregard@alumni.ou.edu>
2018-03-17 12:56:28 -0400

    update request variable

--- resources/views/visits/requests/show.blade.php
+++ resources/views/visits/requests/show.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<h2>{{ $visit->client->fullname }} <a href="{{ route('visits.show', $visit->id) }}">({{ $visit->date->format('m/d/Y') }})</a> Request: {{ $visitrequest->formattedType }} <a href="{{ route('visits.requests.edit', [$visit->id, $visitrequest->id]) }}" class="glyphicon glyphicon-pencil no-underline"></a></h2>
+<h2>{{ $visit->client->fullname }} <a href="{{ route('visits.show', $visit->id) }}">({{ $visit->date->format('m/d/Y') }})</a> Request: {{ $request->formattedType }} <a href="{{ route('visits.requests.edit', [$visit->id, $request->id]) }}" class="glyphicon glyphicon-pencil no-underline"></a></h2>
 
 
 <div style="margin:14px;">
-    <div class="family-form" data-id="{{ $visitrequest->id ?? 0 }}">
+    <div class="family-form" data-id="{{ $request->id ?? 0 }}">
         <div class="form-group row">
             <div class="col-md-2">
                 <label for="relationship">Type</label>
             </div>
             <div class="col-md-2">
-                <p>{{ $visitrequest->formattedType }}</p>
+                <p>{{ $request->formattedType }}</p>
             </div>
         </div>
 
@@ -20,7 +20,7 @@
                 <label for="name">Action</label>
             </div>
             <div class="col-md-5">
-                <p>{{ $visitrequest->action }}</p>
+                <p>{{ $request->action }}</p>
             </div>
         </div>
 
@@ -29,7 +29,7 @@
                 <label for="name">Amount</label>
             </div>
             <div class="col-md-5">
-                <p>${{ $visitrequest->amount }}</p>
+                <p>${{ $request->amount }}</p>
             </div>
         </div>
 
@@ -38,7 +38,7 @@
                 <label for="name">Notes</label>
             </div>
             <div class="col-md-5">
-                <p>{{ $visitrequest->notes }}</p>
+                <p>{{ $request->notes }}</p>
             </div>
         </div>
 
